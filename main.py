ccb57549273e9f4d62f8a11c9b2e23caf1827ad4
layz <99089467+itzlayz@users.noreply.github.com>
2023-05-02 23:57:03 +0600

    added detect to message context

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import disnake
 import requests
 import speech_recognition as sr
+from time import time
 from pydub import AudioSegment
 from disnake.ext import commands
 from config import token, prefix
@@ -11,9 +12,43 @@ bot = commands.Bot(command_prefix=prefix, intents=disnake.Intents.all())
 @bot.event
 async def on_ready():
     print(f'Bot connected as {bot.user}')
+    
+@client.message_command()
+async def detectVoiceText(inter: disnake.Interaction, msg: disnake.message.Message):
+    if not msg.attachments:
+        return await inter.response.send_message('There is no voice message', ephemeral=True)
+    attachment = msg.attachments[0].url
+    response = requests.get(attachment)
+    tm = str(round(time()))
+
+    print(type(inter), type(msg))
+    
+    with open(f'{tm}.ogg', 'wb') as f:
+        f.write(response.content)
+
+    audio = AudioSegment.from_file(f'{tm}.ogg', format='ogg')
+    audio.export(f'{tm}.wav', format='wav')
+
+    r = sr.Recognizer()
+    with sr.AudioFile(f'{tm}.wav') as source:
+        audio = r.record(source)
+    
+    #without it will be error (like timeout)
+    await inter.response.send_message(f'Text from voice: Loading...')
+    try:
+        # you can change language, but it doesn't matter (maybe faster)
+        text = r.recognize_google(audio, language='ru-RU')
+
+        await inter.edit_original_response(f'Text from voice: {text}')
+        os.remove(f'{tm}.ogg')
+        os.remove(f'{tm}.wav')
+    except sr.UnknownValueError:
+        await inter.edit_original_response(f'Text not recognized')
 
 @bot.command()
 async def detect(ctx):
+    if not ctx.message.reference:
+        return await ctx.send('There is not a voice message')
     attachment = ctx.message.reference.resolved.attachments[0].url
     response = requests.get(attachment)
     name = ctx.author.name
